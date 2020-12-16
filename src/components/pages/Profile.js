95c844c683f892f2e336e0b66bb504b322f95c01
Pavit <pavitpim@gmail.com>
2020-12-16 20:07:09 +0700

    use jwt decode

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,22 +1,34 @@
 import { Button } from 'antd';
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import LocalStorageService from '../../services/localStorageService'
 import {Link} from 'react-router-dom'
+import jwtDecode from 'jwt-decode'
 
 export default function Profile(props) {
+    const [name,setName] = useState("");
+    const [id,setId] = useState(0);
+
     const logout = () => {
         LocalStorageService.removeToken();
         props.setRole("guest");
     }
+    useEffect(()=>{
+        const token = LocalStorageService.getToken();
+        if(token){
+            const user = jwtDecode(token);
+            setName(user.name);
+            setId(user.id);
+        }
+    },[])
     return (
         <div>
             <h2>
                 Profile Page
             </h2>
             <p>
-                <strong>Name:</strong> Sonter
+                <strong>Name:</strong> {name}
                 <br />
-                <strong>User ID:</strong> 15
+                <strong>User ID:</strong> {id}
             </p>
             <Link to="/todo"><Button>Go to Todo List</Button></Link>
             <br/>
